f4ed83b8217c02122cc66d733eb7b29fed883d09
Ghazzooo <elghazawy1996@gmail.com>
2023-01-25 21:04:06 +0200

    service

--- service_account.tf
+++ service_account.tf
@@ -7,23 +7,23 @@ resource "google_project_iam_custom_role" "my_role" {
      "container.nodes.list" ]
 }
 
-resource "google_service_account" "project-service-account" {
+resource "google_service_account" "project_service_account" {
   account_id = "project-service-account"
   project = "my-project-ghazooo"
 }
 
-resource "google_project_iam_binding" "service-account-iam-1" {
+resource "google_project_iam_binding" "service_account_iam_1" {
   project = "my-project-ghazooo"
   role    = "roles/storage.admin"
   members = [
-    "serviceAccount:${google_service_account.project-service-account.email}"
+    "serviceAccount:${google_service_account.project_service_account.email}"
   ]
 }
 
-resource "google_project_iam_binding" "service-account-iam-1" {
+resource "google_project_iam_binding" "service_account_iam_2" {
   project = "my-project-ghazooo"
-  role    = "projects/${google_service_account.project-service-account.project}/roles/${google_project_iam_custom_role.my_role.role_id}"
+  role    = "projects/${google_service_account.project_service_account.project}/roles/${google_project_iam_custom_role.my_role.role_id}"
   members = [
-    "serviceAccount:${google_service_account.project-service-account.email}"
+    "serviceAccount:${google_service_account.project_service_account.email}"
   ]
 }
