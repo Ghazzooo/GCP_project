ea1d0e1cf4d38ce5341a6c365b797ee41d9f091a
Ghazzooo <elghazawy1996@gmail.com>
2023-01-25 20:25:11 +0200

    service_account

---
+++ service_account.tf
@@ -0,0 +1,29 @@
+resource "google_project_iam_custom_role" "my_role" {
+  role_id = "myRole"
+  title = "my role"
+  permissions = ["resourcemanager.projects.get", "storage.buckets.get", "storage.buckets.list" , "storage.objects.get" , "storage.objects.list" ,
+   "container.deployments.get" , "container.deployments.create" , "container.deployments.list" , "container.services.list" , "container.services.get" ,
+    "container.services.create" , "container.clusters.list" , "container.clusters.getCredentials" , "container.clusters.get" , "container.pods.list" ,
+     "container.nodes.list" ]
+}
+
+resource "google_service_account" "project-service-account" {
+  account_id = "project-service-account"
+  project = "my-project-ghazooo"
+}
+
+resource "google_project_iam_binding" "project-service-account-iam" {
+  project = "my-project-ghazooo"
+  role    = "roles/storage.admin"
+  members = [
+  "serviceAccount:${google_service_account.project-service-account.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "project-service-account-iam2" {
+  project = "my-project-ghazooo"
+  role    = "projects/${google_service_account.project-service-account.project}/roles/${google_project_iam_custom_role.my_role.role_id}"
+  members = [
+    "serviceAccount:${google_service_account.project-service-account.email}"
+  ]
+}
